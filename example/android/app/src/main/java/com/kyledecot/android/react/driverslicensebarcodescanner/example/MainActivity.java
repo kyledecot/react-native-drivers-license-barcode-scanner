17278ad750dcd06730d35aa5e6cdd8d7d4025168
Kyle Decot <kyle@joinroot.com>
2018-04-22 17:56:43 -0400

    Android studio stuff

--- example/android/app/src/main/java/com/kyledecot/android/react/driverslicensebarcodescanner/example/MainActivity.java
+++ example/android/app/src/main/java/com/kyledecot/android/react/driverslicensebarcodescanner/example/MainActivity.java
@@ -3,10 +3,6 @@ package com.kyledecot.android.react.driverslicensebarcodescanner.example;
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
-  /**
-   * Returns the name of the main component registered from JavaScript.
-   * This is used to schedule rendering of the component.
-   */
   @Override
   protected String getMainComponentName() {
     return "Example";
