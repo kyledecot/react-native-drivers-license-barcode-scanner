2538b42400b8ad4a294a967800337ed5c4900299
Kyle Decot <kyle@joinroot.com>
2018-05-09 17:38:02 -0400

    Full screen camera on iOS Finally!

--- lib/ios/DriversLicenseBarcodeScanner/DriversLicenseBarcodeScannerView.m
+++ lib/ios/DriversLicenseBarcodeScanner/DriversLicenseBarcodeScannerView.m
@@ -1,56 +1,121 @@
-//
-//  DriversLicenseBarcodeScannerView.m
-//  DriversLicenseBarcodeScanner
-//
-//  Created by Kyle Decot on 4/13/18.
-//  Copyright © 2018 Christopher. All rights reserved.
-//
-
 #import "DriversLicenseBarcodeScannerView.h"
 #import "UIView+React.h"
 
 @implementation DriversLicenseBarcodeScannerView {
     NSString *_license;
     BOOL _flash;
-    AVCaptureDevice *_device;
+    AVCaptureDevice *_captureDevice;
+    AVCaptureVideoPreviewLayer *_previewLayer;
+    AVCaptureSession *_captureSession;
+    AVCaptureVideoDataOutput *_captureOutput;
+    AVCaptureDeviceInput *_captureDeviceInput;
 }
 
-- (instancetype)init
-{
+- (instancetype)init {
     if ((self = [super init])) {
+        NSError *error;
+
         _license = @"";
         _flash = FALSE;
+        _captureDevice = [self backCamera];
+        _captureOutput = [self setupCaptureOutput];
+        _captureDeviceInput = [self setupCaptureDeviceInput:_captureDevice error:&error];
+        _captureSession = [self setupCaptureSessionWithDevice:_captureDevice captureOutput:_captureOutput];
+        _previewLayer = [self setupPreviewLayerWithCaptureSession:_captureSession];
         
+        if (error == nil) {
+            [[self layer] addSublayer: _previewLayer];
+        } else {
+            
+        }
     }
     
     return self;
 }
 
-- (void)didMoveToWindow {
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(didRotate:)name:UIDeviceOrientationDidChangeNotification object:nil];
-    // TODO: How/when do I remove this observer?
+- (AVCaptureVideoPreviewLayer *)setupPreviewLayerWithCaptureSession: (AVCaptureSession *)session {
+    AVCaptureVideoPreviewLayer *previewLayer = [[AVCaptureVideoPreviewLayer alloc] initWithSession: session];
 
-    NSLog(@"DID MOVE TO WINDOW");
-    NSLog(@"WINDOW FRAME: %@", NSStringFromCGRect(self.window.frame));
+    [previewLayer setVideoGravity: AVLayerVideoGravityResizeAspectFill];
+
+    return previewLayer;
+}
+
+- (AVCaptureVideoDataOutput *)setupCaptureOutput {
+    AVCaptureVideoDataOutput *captureOutput = [[AVCaptureVideoDataOutput alloc] init];
     
+    [captureOutput setAlwaysDiscardsLateVideoFrames: YES];
+    [captureOutput setSampleBufferDelegate: self queue:dispatch_get_main_queue()];
+
+    NSString *key = (NSString*)kCVPixelBufferPixelFormatTypeKey;
+    NSNumber *value = [NSNumber numberWithUnsignedInt:kCVPixelFormatType_420YpCbCr8BiPlanarVideoRange];
+    NSDictionary *videoSettings = [NSDictionary dictionaryWithObject:value forKey:key];
+
+    [captureOutput setVideoSettings:videoSettings];
     
+    return captureOutput;
+}
+
+- (AVCaptureSession *)setupCaptureSessionWithDevice:(AVCaptureDevice *)device captureOutput:(AVCaptureOutput *)output {
+    NSError *error;
+    AVCaptureSession *captureSession = [[AVCaptureSession alloc] init];
+    AVCaptureDeviceInput *input = [[AVCaptureDeviceInput alloc] initWithDevice: device error: &error];
+
+    if (error == nil) {
+        [captureSession addInput:input];
+        [captureSession addOutput:output];
+        
+        if ([captureSession canSetSessionPreset:AVCaptureSessionPreset1280x720]) {
+            [captureSession setSessionPreset:AVCaptureSessionPreset1280x720];
+            NSLog(@"Set preview port to 1280X720");
+        } else if ([captureSession canSetSessionPreset:AVCaptureSessionPreset640x480]) {
+            [captureSession setSessionPreset:AVCaptureSessionPreset640x480];
+            NSLog(@"Set preview port to 640x480");
+        } else {
+            NSLog(@"I'm not sure what to do here");
+            abort();
+        }
+    } else {
+        NSLog(@"ERROR: %@", [error localizedDescription]);
+    }
+    
+    return captureSession;
+}
+
+- (AVCaptureDeviceInput *)setupCaptureDeviceInput:(AVCaptureDevice *)device error:(NSError **)error {
+    return [[AVCaptureDeviceInput alloc] initWithDevice: device error: error];
+}
+
+- (void)didMoveToWindow {
     [self startCapturing];
 }
 
 - (void)layoutSubviews {
     [super layoutSubviews];
     
-    CGRect frame = [self frame];
-    
-    
+    [self updatePreviewLayer];
 }
 
-- (void) didRotate:(NSNotification *)notificationl
-{
-    UIDeviceOrientation orientation = [[UIDevice currentDevice] orientation];
+- (void)updatePreviewLayer {
+    _previewLayer.frame = [self frame];
     
-    NSLog(@"WE ROTATED");
-    [self startCapturing];
+    // TODO: This doesn't *quite* work as expected. For instance when going from landscape left to landscape right the value will become wrong....
+    switch ([[UIDevice currentDevice] orientation]) {
+        case UIDeviceOrientationPortrait:
+            _previewLayer.connection.videoOrientation = AVCaptureVideoOrientationPortrait;
+            break;
+        case UIDeviceOrientationLandscapeLeft:
+            _previewLayer.connection.videoOrientation = AVCaptureVideoOrientationLandscapeRight;
+            break;
+        case UIDeviceOrientationLandscapeRight:
+            _previewLayer.connection.videoOrientation = AVCaptureVideoOrientationLandscapeLeft;
+            break;
+        case UIDeviceOrientationPortraitUpsideDown:
+            _previewLayer.connection.videoOrientation = AVCaptureVideoOrientationPortraitUpsideDown;
+            break;
+        default:
+            break;
+    }
 }
 
 - (void)setFlash:(BOOL)flash {
@@ -70,17 +135,16 @@
 }
 
 - (void)setTorch:(bool) torchOn {
-    
-    if ([_device isTorchModeSupported: AVCaptureTorchModeOn]) {
+    if ([_captureDevice isTorchModeSupported: AVCaptureTorchModeOn]) {
         NSError *error;
         
-        if ([_device lockForConfiguration:&error]) {
+        if ([_captureDevice lockForConfiguration:&error]) {
             if (torchOn) {
-                [_device setTorchMode:AVCaptureTorchModeOn];
+                [_captureDevice setTorchMode:AVCaptureTorchModeOn];
             } else {
-                [_device setTorchMode:AVCaptureTorchModeOff];
+                [_captureDevice setTorchMode:AVCaptureTorchModeOff];
             }
-            [_device unlockForConfiguration];
+            [_captureDevice unlockForConfiguration];
         } else {
             NSLog(@"ERROR SETTING TORCH: %@", [error localizedDescription]);
         }
@@ -94,79 +158,43 @@
 -(void)startCapturing {
     NSLog(@"Capturing");
 
-    _device = [self backCamera];
-    
-    AVCaptureVideoDataOutput *captureOutput = [[AVCaptureVideoDataOutput alloc] init];
-    
-    [captureOutput setAlwaysDiscardsLateVideoFrames: YES];
-    [captureOutput setSampleBufferDelegate: self queue:dispatch_get_main_queue()];
+  
+        
+//        long processorCount = NSProcessInfo.processInfo.processorCount;
 
-    NSString *key = (NSString*)kCVPixelBufferPixelFormatTypeKey;
-    NSNumber *value = [NSNumber numberWithUnsignedInt:kCVPixelFormatType_420YpCbCr8BiPlanarVideoRange];
-    NSDictionary* videoSettings = [NSDictionary dictionaryWithObject:value forKey:key];
-    [captureOutput setVideoSettings:videoSettings];
+//        UIViewController *vc = [self reactViewController];
     
-    AVCaptureSession *captureSession = [[AVCaptureSession alloc] init];
     
-    NSError *error = nil;
-    AVCaptureDeviceInput *input = [[AVCaptureDeviceInput alloc] initWithDevice:_device error:&error];
-    
-    if (error != nil) {
-        NSLog(@"ERROR: %@", error);
-    } else {
-        [captureSession addInput: input];
-        
-        [captureSession addOutput:captureOutput];
-        
-        if ([captureSession canSetSessionPreset:AVCaptureSessionPreset1280x720]) {
-            NSLog(@"Set preview port to 1280X720");
-        } else if ([captureSession canSetSessionPreset:AVCaptureSessionPreset640x480]) {
-            NSLog(@"Set preview port to 640x480");
-        }
-        
-        long processorCount = NSProcessInfo.processInfo.processorCount;
-
-        UIViewController *vc = [self reactViewController];
-        
-        AVCaptureVideoPreviewLayer *previewLayer = [[AVCaptureVideoPreviewLayer alloc] initWithSession: captureSession];
-        [previewLayer setFrame: [self reactContentFrame]]; // TODO: how do I get full screen in all orientations?
-        [previewLayer setVideoGravity: AVLayerVideoGravityResizeAspectFill];
-        
-        NSLog(@"%@", NSStringFromCGRect(self.frame));
-        
-        [[self layer] addSublayer: previewLayer];
-        
-        [captureSession startRunning];
+        [_captureSession startRunning];
  
 //        NSLog(@"Number of processors \(%s)", ProcessInfo.processInfo.processorCount);
         
-        
-        if (processorCount < 2) {
-            //        do {
-            //            try device.lockForConfiguration()
-            //            device.activeVideoMinFrameDuration = CMTimeMake(1, 15)
-            //        } catch { }
-            //        device.unlockForConfiguration()
-            //        DLog("activeVideoMinFrameDuration: \(device.activeVideoMinFrameDuration)")
-        }
-       
-        //
-        //    availableThreads = min(MAX_THREADS, ProcessInfo.processInfo.processorCount)
-        //    activeThreads = 0
-        //
-        //    prevLayer = AVCaptureVideoPreviewLayer(session: captureSession) as AVCaptureVideoPreviewLayer
-        //
-        //    prevLayer.connection.videoOrientation = AVCaptureVideoOrientation.portrait
-        //    prevLayer.frame = CGRect(x: 0, y: 0, width: min(frame.size.width, frame.size.height), height: max(frame.size.width, frame.size.height))
-        //
-        //    prevLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
-        //
-        //    layer.addSublayer(prevLayer)
-        //
-        //    setupCustomOverlay()
-        //
-        //    focusTimer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(reFocus), userInfo: nil, repeats: true)
-    }
+    
+//    if (processorCount < 2) {
+        //        do {
+        //            try device.lockForConfiguration()
+        //            device.activeVideoMinFrameDuration = CMTimeMake(1, 15)
+        //        } catch { }
+        //        device.unlockForConfiguration()
+        //        DLog("activeVideoMinFrameDuration: \(device.activeVideoMinFrameDuration)")
+//    }
+   
+    //
+    //    availableThreads = min(MAX_THREADS, ProcessInfo.processInfo.processorCount)
+    //    activeThreads = 0
+    //
+    //    prevLayer = AVCaptureVideoPreviewLayer(session: captureSession) as AVCaptureVideoPreviewLayer
+    //
+    //    prevLayer.connection.videoOrientation = AVCaptureVideoOrientation.portrait
+    //    prevLayer.frame = CGRect(x: 0, y: 0, width: min(frame.size.width, frame.size.height), height: max(frame.size.width, frame.size.height))
+    //
+    //    prevLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
+    //
+    //    layer.addSublayer(prevLayer)
+    //
+    //    setupCustomOverlay()
+    //
+    //    focusTimer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(reFocus), userInfo: nil, repeats: true)
 }
 
 - (AVCaptureDevice *)backCamera {
